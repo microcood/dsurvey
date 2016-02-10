68a617b28bc410b64c77721f396d7cf066784c42
Micwaits Microcood <microcood@gmail.com>
2016-02-10 10:24:13 +0300

    Back users to admin

--- app/admin.py
+++ app/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.models import *
 from django.contrib.admin.models import LogEntry
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+# admin.site.unregister(User)
+# admin.site.unregister(Group)
 
 @admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin):
